2eecae98344b5e262c20efe9f06665e97987dd57
Rafe <stefanorafe@gmail.com>
2023-05-24 20:30:19 +0200

    first test

--- flight/tests.py
+++ flight/tests.py
@@ -1,5 +1,7 @@
 # from django.test import TestCase
 from rest_framework.test import APIRequestFactory, APITestCase
+from django.contrib.auth.models import AnonymousUser
+from .views import FlightView
 
 
 # Create your tests here.
@@ -10,4 +12,6 @@ class FlightTestCase(APITestCase):
 
     def test_flight_list_as_guest_user(self):
         request = self.factory.get('/flight/flights/')
-        
+        request.user = AnonymousUser()
+        response = FlightView.as_view({'get': 'list'})(request)
+        self.assertEquals(response.status_code, 200)
