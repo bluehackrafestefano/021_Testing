1541d8c945d1612f859c85a95f8204d622138175
Rafe <stefanorafe@gmail.com>
2023-05-24 20:58:39 +0200

    4ncu test

--- flight/tests.py
+++ flight/tests.py
@@ -1,7 +1,8 @@
 # from django.test import TestCase
 from rest_framework.test import APIRequestFactory, APITestCase
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User
 from .views import FlightView
+from rest_framework.test import force_authenticate
 
 
 # Create your tests here.
@@ -9,6 +10,12 @@ class FlightTestCase(APITestCase):
     
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(
+            username='tarik',
+            email='tarik@gmail.com',
+            password='secret123'
+        )
+
 
     def test_flight_list_as_guest_user(self):
         request = self.factory.get('/flight/flights/')
@@ -22,4 +29,18 @@ class FlightTestCase(APITestCase):
         request.user = AnonymousUser()
         response = FlightView.as_view({'post': 'create'})(request)
         self.assertEqual(response.status_code, 401)
+    
+    
+    def test_flight_create_as_login_user(self):
+        request = self.factory.post('/flight/flights/')
+        force_authenticate(request, user=self.user)
+        response = FlightView.as_view({'post': 'create'})(request)
+        self.assertEqual(response.status_code, 403)
+    
+    
+    def test_flight_create_as_admin_user(self):
+        request = self.factory.post('/flight/flights/')
+        force_authenticate(request, user=self.user)
+        response = FlightView.as_view({'post': 'create'})(request)
+        self.assertEqual(response.status_code, 201)
 
